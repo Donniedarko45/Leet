d353b188fddcc08415e9c998c4dc4201f905b72f
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2026-08-02 11:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.24%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+       // state dp[i] at each step i should maximise the piles till i th index
+       return true;   
+    }
+};
