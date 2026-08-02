fae30c7c5317cad0c4f1120d80971db252259e52
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2026-08-02 12:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (35.41%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>mp1;
+        unordered_map<int,int>mp2;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        vector<int>ans;
+        for(auto it:nums1){
+            mp1[it]++;
+        }
+        
+        for(int i=0;i<n2;i++){
+            if(mp1[nums2[i]]){
+                ans.push_back(nums2[i]);
+                mp1.erase(nums2[i]);
+            }
+        }
+        return ans;
+    }
+};
